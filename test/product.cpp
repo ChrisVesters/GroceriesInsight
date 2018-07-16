b9dda04d6768f7cd8a593b0a26ec9638a9a254e1
Chris Vesters <vesters_chris@hotmail.com>
2018-07-16 08:02:22 +0200

    Issue#1 Add tests for product and product service.

---
+++ test/product.cpp
@@ -0,0 +1,33 @@
+#include "product.h"
+
+TEST(ProductTest, Constructor) {
+	Product testProduct(0, "Test", "Case");
+}
+
+TEST(ProductTest, ConstructorNullBrand) {
+
+}
+
+TEST(ProductTest, ConstructorEmptyBrand) {
+
+}
+
+TEST(ProductTest, ConstructorNullName) {
+
+}
+
+TEST(ProductTest, ConstructorEmptyName) {
+
+}
+
+TEST(ProductTest, GetBarcode) {
+
+}
+
+TEST(ProductTest, GetBrand) {
+
+}
+
+TEST(ProductTest, GetName) {
+
+}
