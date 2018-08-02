a0d99b7be898118046a0ff21f4c9d2f012cdea86
Chris Vesters <vesters_chris@hotmail.com>
2018-08-02 08:59:28 +0200

    Issue#3 Clean up Product Service.
    The current implementation of the persistency is very basic.
    A lot of improvements are required, but some of these depend on a better version of the ObjestSavingFramework.
    There are currently also not test as data is directly written to file and doing file comparisons is pretty nasty with the current state of code.
    This part requires some refactering in the future, wich will be done in Issue#14
    
    Closes #3

--- src/productservice.cpp
+++ src/productservice.cpp
@@ -33,28 +33,18 @@ Product ProductService::getProduct(const int barcode) {
 	throw -1;
 }
 
-const string toString(int value) {
-	char buffer[10];
-	sprintf(buffer, "%d", value);
-	return string(buffer);
-}
-
-const int fromString(string value) {
-	return stoi(value);
-}
-
 void ProductService::save() {
 	DataGatherer gatherer;
 	gatherer.startObject("products");
-	gatherer.addValue("size", toString(products.size()));
+	gatherer.addValue("size", to_string(products.size()));
 	
 	for (int i = 0; i < products.size(); ++i) {
 		Product product = products[i];
-		gatherer.startObject("product-" + toString(i));
-		gatherer.addValue("barcode", toString(product.getBarcode()));
+		gatherer.startObject("product-" + to_string(i));
+		gatherer.addValue("barcode", to_string(product.getBarcode()));
 		gatherer.addValue("brand", product.getBrand());
 		gatherer.addValue("name", product.getName());
-		gatherer.endObject("product-" + toString(i));
+		gatherer.endObject("product-" + to_string(i));
 	}
 	gatherer.endObject("products");
 	
@@ -67,18 +57,18 @@ void ProductService::load() {
 	DataGatherer gatherer = json.load();
 
 	gatherer.loadObject("products");
-	const int size = fromString(gatherer.getValue("size"));
+	const int size = stoi(gatherer.getValue("size"));
 	
 	for (int i = 0; i < size; ++i) {
-		gatherer.loadObject("product-" + toString(i));
-		const int barcode = fromString(gatherer.getValue("barcode"));
+		gatherer.loadObject("product-" + to_string(i));
+		const int barcode = stoi(gatherer.getValue("barcode"));
 		const string brand = gatherer.getValue("brand");
 		const string name = gatherer.getValue("name");
 		
 		Product product(barcode, brand, name);
 		addProduct(product);
 		
-		gatherer.releaseObject("product-" + toString(i));
+		gatherer.releaseObject("product-" + to_string(i));
 	}
 	gatherer.releaseObject("products");
 }
